7b54d93cb7a227994613d5cebf3e707974bb6ff8
Zane Beckwith <zanebeckwith@gmail.com>
2014-08-04 10:49:13 -0500

    Set up skeleton of scanner function.
    Just the private data and public functions,
    but the actual logic of the constructor,
    which does the actual scanning,
    is just a piece of pseudocode right now.

--- parser.js
+++ parser.js
@@ -10,6 +10,32 @@ Parse.Token = function(type_in, rep_in) {
         return that;
 };
 
+// Scanner type, for tokenizing input line character stream.
+// Constructor function tokenizes the given input.
+// Methods pop and peak provide stack-like access to
+// resulting token stream.
+Parse.Scanner = function(line) {
+        var that = {};
+
+        var toks = [];  // Internal representation of token stream
+
+        // Set up simple finite automaton to classify tokens
+        // and determine their representation,
+        // and save each to the toks array.
+
+        // Function to view (but don't delete) next token
+        that.peak = function() {
+                return toks[toks.length - 1];
+        };
+
+        // Get and delete next token
+        that.pop = function() {
+                return toks.pop();
+        };
+
+        return that;
+};
+
 function parseLine(input) {
         return input;
 };
