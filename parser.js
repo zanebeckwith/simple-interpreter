24e72203e67b6c5defd6c4df1d98989cd4abc901
Zane Beckwith <zanebeckwith@gmail.com>
2014-08-04 10:24:41 -0500

    Create token type in parser.js.

--- parser.js
+++ parser.js
@@ -1,5 +1,15 @@
 var Parse = {}; // File namespace
 
+// Token type, for storing info on type and represention
+// of lexical tokens in input line.
+Parse.Token = function(type_in, rep_in) {
+        var that = {};
+        that.type = type_in;
+        that.rep = rep_in;
+
+        return that;
+};
+
 function parseLine(input) {
         return input;
 };
