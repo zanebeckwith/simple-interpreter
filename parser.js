82ad371a052c1479b57f8fc6a546052921246eb5
Zane Beckwith <zanebeckwith@gmail.com>
2014-08-05 18:56:26 -0500

    Implement code generation logic in parser.
    This involves threading registers throughout
    the recursive function calls, so each piece can
    save its results (and sometimes combine with
    previous results).
    
    As before, there is essentially no error handling here yet.

--- parser.js
+++ parser.js
@@ -116,10 +116,14 @@ Parse.Scanner = function(line) {
 // Function to parse given token stream
 Parse.Parse = function(tokens) {
         var code = [];  // Holds code to be executed on vm
+        var result;     // Holds result of computation
 
         if (Parse.line(tokens, code)) {
                 if (tokens.peak().type === 'eof') {
-                        return Parse.VM.run(code);
+                        code.push({op: 'stop'});
+                        result = Parse.vm.run(code);
+                        Parse.vm.clearRegisters();
+                        return result;
                 } else {
                         Parse.reportFail();
                         return false;
@@ -131,40 +135,129 @@ Parse.Parse = function(tokens) {
 };
 
 Parse.line = function(tokens, code) {
+        var res = Parse.vm.newRegister();        // Register in which to save results
+        var varName;    // Name of new variable, if line is a definition
+
+        // Check if a definition, or just an expression
         if (tokens.peak().type === 'def') {
                 tokens.pop();
-                tokens.pop();
-                tokens.pop();
-                return Parse.expr(tokens, code);
+                if (tokens.peak().type === 'ident') {
+                        // If it's a new variable,
+                        //      register it in the symbol table,
+                        //      and allocate storage for it
+                        if (!(tokens.peak().rep in Parse.symTab)) {
+                                Parse.symTab[tokens.peak().rep] = {};
+                                Parse.symTab[tokens.peak().rep].loc =
+                                        Parse.vm.allocate(1);
+                        }
+
+                        varName = tokens.pop().rep;
+                        if (tokens.pop().type !== 'equals') {
+                                Parse.reportFail();
+                                return false;
+                        };
+
+                        // Parse the rhs
+                        if (Parse.expr(tokens, code, res)) {
+                                // Register the code to save the value
+                                code.push({op: 'store',
+                                           mem: Parse.symTab[varName].loc,
+                                           reg: res
+                                });
+
+                                return true;
+                        } else {
+                                Parse.reportFail();
+                                return false;
+                        };
+                } else {
+                        Parse.reportFail();
+                        return false;
+                };
         } else {
-                return Parse.expr(tokens, code);
+                return Parse.expr(tokens, code, res);
         };
 };
 
-Parse.expr = function(tokens, code) {
-        if (Parse.term(tokens, code)) {
-                return Parse.exprP(tokens, code);
+Parse.expr = function(tokens, code, res) {
+        // Register in which to save result of term
+        var termReg = Parse.vm.newRegister();
+
+        // Register in which to save result of exprP
+        // exprP also requires we give it termReg,
+        //      so it can combine it with its sub-parts
+        var exprPReg = Parse.vm.newRegister();
+
+        if (Parse.term(tokens, code, termReg)) {
+                if (Parse.exprP(tokens, code, termReg, exprPReg)) {
+                        // Copy results into res register
+                        code.push({op: 'mov', reg1: exprPReg, reg2: res});
+
+                        return true;
+                } else {
+                        return false;
+                };
         } else {
                 Parse.reportFail();
                 return false;
         };
 };
 
-Parse.term = function(tokens, code) {
-        if (Parse.factor(tokens, code)) {
-                return Parse.termP(tokens, code);
+Parse.term = function(tokens, code, res) {
+        // Register in which to save result of factor
+        var factorReg = Parse.vm.newRegister();
+
+        // Register in which to save result of termP
+        // termP also requires we give it factorReg,
+        //      so it can combine it with its sub-parts
+        var termPReg = Parse.vm.newRegister();
+
+        if (Parse.factor(tokens, code, factorReg)) {
+                if (Parse.termP(tokens, code, factorReg, termPReg)) {
+                        // Copy results into res register
+                        code.push({op: 'mov', reg1: termPReg, reg2: res});
+
+                        return true;
+                } else {
+                        Parse.reportFail();
+                        return false;
+                };
         } else {
                 Parse.reportFail();
                 return false;
         };
 };
 
-Parse.exprP = function(tokens, code) {
+Parse.exprP = function(tokens, code, leftReg, res) {
+        // Register in which to save result of term
+        var termReg = Parse.vm.newRegister();
+
+        // Register in which to save result of combining
+        var combReg = Parse.vm.newRegister();
+
+        // Register in which to save result of exprP
+        var exprPReg = Parse.vm.newRegister();
+
         if (tokens.peak().rep === '+') {
                 tokens.pop();
 
-                if (Parse.term(tokens, code)) {
-                        return Parse.exprP(tokens, code);
+                if (Parse.term(tokens, code, termReg)) {
+                        // Add results of previous parsing and
+                        //      the result of term
+                        code.push({op: 'add',
+                                   reg1: leftReg,
+                                   reg2: termReg,
+                                   dest: combReg
+                        });
+                        if (Parse.exprP(tokens, code, combReg, exprPReg)) {
+                                // Copy results into res register
+                                code.push({op: 'mov', reg1: exprPReg, reg2: res});
+
+                                return true;
+                        } else {
+                                Parse.reportFail();
+                                return false;
+                        };
                 } else {
                         Parse.reportFail();
                         return false;
@@ -172,14 +265,31 @@ Parse.exprP = function(tokens, code) {
         } else if (tokens.peak().rep === '-') {
                 tokens.pop();
 
-                if (Parse.term(tokens, code)) {
-                        return Parse.exprP(tokens, code);
+                if (Parse.term(tokens, code, termReg)) {
+                        // Subtract results of previous parsing and
+                        //      the result of term
+                        code.push({op: 'sub',
+                                   reg1: leftReg,
+                                   reg2: termReg,
+                                   dest: combReg
+                        });
+                        if (Parse.exprP(tokens, code, combReg, exprPReg)) {
+                                // Copy results into res register
+                                code.push({op: 'mov', reg1: exprPReg, reg2: res});
+
+                                return true;
+                        } else {
+                                Parse.reportFail();
+                                return false;
+                        };
                 } else {
                         Parse.reportFail();
                         return false;
                 }
         } else if (tokens.peak().type === 'rParens'
                         || tokens.peak().type === 'eof') {
+                // Copy unchanged left side into res register
+                code.push({op: 'mov', reg1: leftReg, reg2: res});
                 return true;
         } else {
                 Parse.reportFail();
@@ -187,12 +297,36 @@ Parse.exprP = function(tokens, code) {
         };
 };
 
-Parse.termP = function(tokens, code) {
+Parse.termP = function(tokens, code, leftReg, res) {
+        // Register in which to save result of factor
+        var factorReg = Parse.vm.newRegister();
+
+        // Register in which to save result of combining
+        var combReg = Parse.vm.newRegister();
+
+        // Register in which to save result of termP
+        var termPReg = Parse.vm.newRegister();
+
         if (tokens.peak().rep === '*') {
                 tokens.pop();
 
-                if (Parse.factor(tokens, code)) {
-                        return Parse.termP(tokens, code);
+                if (Parse.factor(tokens, code, factorReg)) {
+                        // Multiply results of previous parsing and
+                        //      the result of factor
+                        code.push({op: 'mult',
+                                   reg1: leftReg,
+                                   reg2: factorReg,
+                                   dest: combReg
+                        });
+                        if (Parse.termP(tokens, code, combReg, termPReg)) {
+                                // Copy results into res register
+                                code.push({op: 'mov', reg1: termPReg, reg2: res});
+
+                                return true;
+                        } else {
+                                Parse.reportFail();
+                                return false;
+                        };
                 } else {
                         Parse.reportFail();
                         return false;
@@ -200,8 +334,23 @@ Parse.termP = function(tokens, code) {
         } else if (tokens.peak().rep === '/') {
                 tokens.pop();
 
-                if (Parse.factor(tokens, code)) {
-                        return Parse.termP(tokens, code);
+                if (Parse.factor(tokens, code, factorReg)) {
+                        // Divide results of previous parsing and
+                        //      the result of factor
+                        code.push({op: 'div',
+                                   reg1: leftReg,
+                                   reg2: factorReg,
+                                   dest: combReg
+                        });
+                        if (Parse.termP(tokens, code, combReg, termPReg)) {
+                                // Copy results into res register
+                                code.push({op: 'mov', reg1: termPReg, reg2: res});
+
+                                return true;
+                        } else {
+                                Parse.reportFail();
+                                return false;
+                        };
                 } else {
                         Parse.reportFail();
                         return false;
@@ -210,6 +359,8 @@ Parse.termP = function(tokens, code) {
                         tokens.peak().rep === '+' ||
                         tokens.peak().rep === '-' ||
                         tokens.peak().type === 'eof') {
+                // Copy unchanged left side into res register
+                code.push({op: 'mov', reg1: leftReg, reg2: res});
                 return true;
         } else {
                 Parse.reportFail();
@@ -217,11 +368,11 @@ Parse.termP = function(tokens, code) {
         };
 };
 
-Parse.factor = function(tokens, code) {
+Parse.factor = function(tokens, code, res) {
         if (tokens.peak().type === 'lParens') {
                 tokens.pop();
 
-                if (!Parse.expr(tokens, code)) {
+                if (!Parse.expr(tokens, code, res)) {
                         Parse.reportFail();
                         return false;
                 }
@@ -233,8 +384,17 @@ Parse.factor = function(tokens, code) {
 
                 tokens.pop();
                 return true;
-        } else if (tokens.peak().type === 'num' ||
-                        tokens.peak().type === 'ident') {
+        } else if (tokens.peak().type === 'num') {
+                // Load the constant number into the res register
+                code.push({op: 'loadImm', reg: res, imm: tokens.peak().rep});
+                tokens.pop();
+                return true;
+        } else if (tokens.peak().type === 'ident') {
+                // Load the saved value into the res register
+                code.push({op: 'load',
+                           reg: res,
+                           mem: Parse.symTab[tokens.peak().rep].loc
+                });
                 tokens.pop();
                 return true;
         } else {
@@ -254,9 +414,3 @@ function parseLine(input) {
         return Parse.Parse(tokens);
 };
 
-//         Parse.symTab.x = {};
-//         Parse.symTab.x.loc = Parse.vm.allocate(1);
-//         Parse.symTab.x.val = 20;
-//         Parse.vm.run([{op: 'storeImm', mem: Parse.symTab.x.loc, imm: Parse.symTab.x.val},
-//                       {op: 'stop'}
-//                       ]);
