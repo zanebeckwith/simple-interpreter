835cbe581a8c675767dae5a715510f825c1087d7
Zane Beckwith <zanebeckwith@gmail.com>
2014-08-05 12:26:01 -0500

    Add simple virtual machine.
    In a new file, virtual-machine.js, a new
    VirtualMachine type is defined.
    
    It can execute simple assembly-like instructions:
            Load/Load-Immediate
            Store/Store-Immediate
            Add, Subtract, Multiply, and Divide
    
    It has a bank of registers (unlimited in number),
    and a block of memory.
    Both of these things are represented as arrays.
    
    Instructions can be given to the machine in the format
    {opcode: <op>, reg: <reg>, mem: <mem>, ...}
    where the exact configuration of registers and memory locations
    varies according to the specific instruction.
    An instruction with opcode: 'stop' will terminate the execution
    of an array of instructions, at which point the run method
    returns with the value of the 0-register.
    The machine retains state (registers and memory, as well as
    heap and stack pointers), after this termination, though.
    
    Memory can be allocated using an allocate method.
    It increments the heap pointer, and returns the location
    of the allocated memory for later reference.
    
    TODO:
            Implement stack operations (push/pop etc.)
            Implement logical tests for branching and looping
            Implement jumps for branching and function calling

---
+++ virtual-machine.js
@@ -0,0 +1,71 @@
+// Namespace for this file
+VM = {};
+
+// Virtual-machine type, for interpreting bytecode-like instructions
+VM.VirtualMachine = function() {
+        var that = {};
+
+        var registers = [];     // Unlimited number of registers
+        var memory = [];        // von Neumann architecture memory
+        var pc = 0;             // Program counter
+        var heap = 1e3;         // Heap pointer (code can have <=1000 instructions)
+        var stack = 1e9;        // Stack pointer (heap can grow by 1e6
+                                //      before crashing into stack)
+        var opCode;
+
+        // Run given code.
+        that.run = function(code) {
+                // Load code
+                for (pc = 0; pc < code.length; ++pc) {
+                        memory[pc] = code[pc];
+                };
+
+
+                // Run through code
+                for (pc = 0; (opCode = (memory[pc]).op) !== 'stop'; ++pc) {
+                        switch (opCode) {
+                                case 'load': 
+                                        registers[memory[pc].reg] = memory[memory[pc].mem];
+                                        break;
+                                case 'loadImm': 
+                                        registers[memory[pc].reg] = memory[pc].imm;
+                                        break;
+                                case 'store':
+                                        memory[memory[pc].mem] = registers[memory[pc].reg];
+                                        break;
+                                case 'storeImm':
+                                        memory[memory[pc].mem] = memory[pc].imm;
+                                        break;
+                                case 'add':
+                                        registers[memory[pc].dest] =
+                                                registers[memory[pc].reg1] + registers[memory[pc].reg2];
+                                        break;
+                                case 'sub':
+                                        registers[memory[pc].dest] =
+                                                registers[memory[pc].reg1] - registers[memory[pc].reg2];
+                                        break;
+                                case 'mult':
+                                        registers[memory[pc].dest] =
+                                                registers[memory[pc].reg1] * registers[memory[pc].reg2];
+                                        break;
+                                case 'div':
+                                        registers[memory[pc].dest] =
+                                                registers[memory[pc].reg1] / registers[memory[pc].reg2];
+                                        break;
+                                default:
+                                        // Just don't do anything on unknown opcode
+                        };
+                };
+
+                return registers[0];
+        };
+
+        // Allocate storage
+        that.allocate = function(size) {
+                heap += size;
+                return heap - size;
+        };
+
+        return that;
+};
+
